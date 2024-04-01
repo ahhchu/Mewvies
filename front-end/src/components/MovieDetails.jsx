712b865bcbb7117a79e74bde3f7354dd21440e92
gargee-jams <104339886+gargee-jams@users.noreply.github.com>
2024-04-01 02:10:54 -0400

    import statement that keeps breaking site removed

--- front-end/src/components/MovieDetails.jsx
+++ front-end/src/components/MovieDetails.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import React, { useState, useEffect } from "react";
 import "./MovieDetails.css";
 import MovieCard from "./MovieCard";
 
