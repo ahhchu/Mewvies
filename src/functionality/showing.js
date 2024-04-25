394208a9dc5e707042e95812109613d0a6056130
Helium Yang <47203690+HeliumYang@users.noreply.github.com>
2024-04-25 19:17:54 -0400

    Showing done, ticketing separated out from order

--- src/functionality/showing.js
+++ src/functionality/showing.js
@@ -1,48 +1,95 @@
-//addShowing, removeShowing, getShowings, getShowingsMovie checkAvailableSeats, assignTicket, removeTicket
+import { getRooms } from "./showrooms";
 
-export async function addShowing(roomName, roomRows, roomColumns) {
-    var newRoom = {};
+/* Adds a showing. Everything is a string, except showingTime, which needs to be a Date()
+ */
+export async function addShowing(movieID, roomID, showingTime) {
+    var newShowing = {};
     try {
-        newRoom = {
-            room_name: roomName,
-            room_rows: roomRows,
-            room_col: roomColumns,
-            room_id: 0,
-            room_capacity: roomRows * roomColumns,
+        if (checkShowingAvailability(roomID, showingTime)) {
+        newShowing = {
+            movie_id: movieID,
+            room_id: roomID,
+            showing_time: showingTime,
+            showing_id: 0,
         };
         getRooms().then(async (data) => {
-            newRoom.room_id = data.length;
-            var roomRef = doc(db, "show_rooms", newRoom.room_id.toString());
-            await setDoc(roomRef, newRoom);
+            newShowing.showing_id = data.length;
+            var showingRef = doc(db, "showing", newShowing.showing_id.toString());
+            await setDoc(showingRef, newShowing);
             return true;
         })
+    }
     } catch (error) {
         console.log(error);
       } // try
-} // addRoom
+} // addShowing
 
+/* gets all showings, no param required.
+*/
 export async function getShowings() {
     try {
-        var snapshot = await getDocs(collection(db, "show_rooms"));
-        var existingRooms = [];
+        var snapshot = await getDocs(collection(db, "showing"));
+        var existingShowings = [];
         snapshot.docs.forEach((element) => {
-            existingRooms.push(element.data());
+            existingShowings.push(element.data());
         });
-        return existingRooms;
+        return existingShowings;
     } catch (error) {
         return [];
     } // try
-} // getRooms
+} // getShowings
 
-export async function removeShowing(roomID) {
+/* gets all showings by movieID
+*/
+export async function getShowingsByMovie(movieID) {
+    try {
+        var snapshot = await getDocs(collection(db, "showing"));
+        var existingShowings = [];
+        snapshot.docs.forEach((element) => {
+            if (element.movie_id == movieID) {
+                existingShowings.push(element.data());
+            }
+        });
+        return existingShowings;
+    } catch (error) {
+        return [];
+    } // try
+} // getShowings
+
+/* Internal function used for the sake of checking if a time & room combination is available.
+ */
+async function checkShowingAvailability(roomID, showingTime) {
+    try {
+        // checks if the room is available at the specified time
+        var snapshot = await getDocs(collection(db, "showing"));
+        snapshot.docs.forEach((element) => {
+            if (element.room_id == roomID && element.showing_time.getTime() == showingTime) {
+                return false;
+            }
+        });
+
+        // checks if the room exists.
+        var rooms = await getRooms();
+        rooms.forEach(element => {
+            if (element.room_id == roomID) {
+                return true;
+            }
+        });
+        return false;
+    } catch (error) {
+        return false;
+    } // try
+} // checkShowingAvailability
+
+export async function removeShowing(showingID) {
     var promise;
-    var snapshot = await getDocs(collection(db, "show_rooms"));
+    var snapshot = await getDocs(collection(db, "showing"));
     if (snapshot.docs.length == 0) {
         return;
     }
         snapshot.docs.forEach((element) => {
-            if (element.data().room_id == roomID) {
+            if (element.data().showing_id == showingID) {
                 deleteDoc(element.ref, promise);
             } // if
         });
-}
+} // removeShowing
---
+++ src/functionality/ticketing.js
@@ -0,0 +1,1 @@
+//checkAvailableSeats, assignTicket, removeTicket
